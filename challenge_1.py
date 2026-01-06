dbb2986783da5727c199b250e038e4e259b5f8ee
Asad Rzayev <rzasad522@gmail.com>
2026-01-06 16:22:25 +0400

    Create challenge_1.py

---
+++ challenge_1.py
@@ -0,0 +1,15 @@
+/*
+Author: Asad Rzayev
+Purpose: This program prints out my name to the screen
+Date: 08/12/2024
+*/
+ 
+#include <stdio.h>
+ 
+int main()
+{
+    // this code is displaying my name
+    printf("Hi, my name is Asad.");
+    
+    return 0;
+}
